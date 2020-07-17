b8f43674ede75a2e1d885fd58a1b817040bf71e5
666 <daryana180905@gmail.com>
2020-07-17 14:21:54 +0300

    4 upload

--- roarroar.js
+++ roarroar.js
@@ -1,30 +1,64 @@
 let DB = {
     1:{},
-    2:{},
     3:{},
+    2:{},
     4:{}
 };
-let pass='';
+let pass = 0;
 let key;
 let num;
 let ok=true;
 let bet;
+// function g() {
+//     while (pass!=null) {
+//         pass = prompt("pass?");
+//         for(key in DB){
+//             if (key==pass) {
+//                 while (ok==true) {
+//                     num=prompt('number?');
+//                     bet=+prompt('bet?');
+//                     ok=confirm('one more?');
+//                     console.log(DB);
+//                     console.log(Coins);
+//                     
+//                 }
+//             }
+//         }
+//     }
+// }
 
-function g() {
-    while (pass!=null) {
-        pass = prompt("pass?");
+function gg() {
+    pass = +prompt("your password:");
+    while(pass != null){
         for(key in DB){
             if (key==pass) {
-                while (ok==true) {
-                    num=prompt('number?');
-                    bet=+prompt('bet?');
-                    ok=confirm('one more?');
+                num = +prompt('number?');
+                bet = +prompt('bet?');
+                ok=confirm('one more?');
+                DB[key][num] = bet;
+                console.log(DB);
+                if (ok==false) {
+                    pass = +prompt("your password:");
                     console.log(DB);
-                    console.log(Coins);
                     
+                }else{
+                    // g();
+                    num = +prompt('number?');
+                    bet = +prompt('bet?');
+                    ok=confirm('one more?');
+                    DB[key][num] = bet;
+                    console.log(DB);
                 }
+                console.log(DB);
+                // if (ok == true) {g();}
             }
         }
     }
 }
-
+// function g(){
+//     num = +prompt('number?');
+//     bet = +prompt('bet?');
+//     ok=confirm('one more?');
+//     DB[key][num] = bet;
+//     console.log(DB);
+// }
